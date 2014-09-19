6fb190a8ab4fe46102acdfc903bce2e30b6627c3
ecarmichael <ecarmichael@gmail.com>
2014-09-19 15:53:37 -0400

    Added a transpose section for data that is not Nx1
    Amended an error cause by an iv.tstart/iv.tend that was not Nx1 and thus
    could not be concatenated using cat(1,..).  If the iv is 1xN, then it is
    transposed.

--- code-matlab/shared/datatypes/iv/UnionIV.m
+++ code-matlab/shared/datatypes/iv/UnionIV.m
@@ -6,15 +6,23 @@ function iv1 = UnionIV(cfg_in,iv1,iv2)
 % output is resorted based on start times (ascending)
 %
 % MvdM 2014-08-28 initial version
+% EC 2014-09-28 
+%      Added a correction incase the dimesions were not correct
+%      for the cat(1,...) command.  If this is the case then iv1 and iv2 
+%      are transposed to allow for veritcal concatination.  
 
 cfg = [];
 ProcessConfig;
 
 mfun = mfilename;
-
-iv1.tstart = cat(2,iv1.tstart,iv2.tstart);
-iv1.tend = cat(2,iv1.tend,iv2.tend);
-
+if length(iv1.tstart)>1 && length(iv2.tstart)>1
+    iv1.tstart = cat(1,iv1.tstart',iv2.tstart');
+    iv1.tend = cat(1,iv1.tend',iv2.tend');
+else
+    
+    iv1.tstart = cat(1,iv1.tstart,iv2.tstart);
+    iv1.tend = cat(1,iv1.tend,iv2.tend);
+end
 [iv1.tstart,sort_idx] = sort(iv1.tstart,'ascend');
 iv1.tend = iv1.tend(sort_idx);
 
