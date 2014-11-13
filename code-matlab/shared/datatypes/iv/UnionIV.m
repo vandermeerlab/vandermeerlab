b3aea9bd24cfd8eb28eae4660a2c36829a7878af
mvdm <mvdm@uwaterloo.ca>
2014-11-12 21:13:25 -0500

    Reverted UnionIV() back to using column vectors

--- code-matlab/shared/datatypes/iv/UnionIV.m
+++ code-matlab/shared/datatypes/iv/UnionIV.m
@@ -12,8 +12,10 @@ cfg = ProcessConfig2(cfg_def,cfg_in);
 
 mfun = mfilename;
 
-iv1.tstart = cat(2,iv1.tstart,iv2.tstart);
-iv1.tend = cat(2,iv1.tend,iv2.tend);
+% should add a CheckIV function here
+
+iv1.tstart = cat(1,iv1.tstart,iv2.tstart); % note that constructor guarantees column vectors
+iv1.tend = cat(1,iv1.tend,iv2.tend);
 
 [iv1.tstart,sort_idx] = sort(iv1.tstart,'ascend');
 iv1.tend = iv1.tend(sort_idx);
