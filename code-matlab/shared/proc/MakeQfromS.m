c8239812ce0596b5755be107fb463c234b2e742a
mvdm <mvdm at uwaterloo>
2015-02-01 17:58:40 -0500

    updates to MakeQFromS()
    *added smoothing option
    *updated to ProcessConfig2()

--- code-matlab/shared/proc/MakeQfromS.m
+++ code-matlab/shared/proc/MakeQfromS.m
@@ -1,20 +1,35 @@
 function Q = MakeQfromS(cfg_in,S)
 % function Q = MakeQfromS(cfg,S)
 %
-% Make Q matrix from ts with spike data
+% Make "Q-matrix" (spike counts in [nCells x nTimeBins] matrix) from ts with spike data
 %
 % INPUTS:
 %
+% S: ts with spikes
+%
+% CONFIG OPTIONS:
+%
+% cfg_def = [];
+% cfg_def.dt = 0.05;
+% cfg_def.smooth = 0;
+% cfg_def.gausswin_size = 1; % in seconds; only used if cfg.smooth = 1
+% cfg_def.gausswin_sd = 0.02; % in seconds
+% cfg_def.tvec_edges = [];
+% 
 % OUTPUTS:
+%
+% Q: [nCells x nTimeBins] tsd with spike counts
 %
 % MvdM 2014-08-21
 
-cfg = [];
-cfg.dt = 0.05;
-cfg.mode = 'spikeCount'; % 'firingRate'
-cfg.tvec_edges = [];
+cfg_def = [];
+cfg_def.dt = 0.05;
+cfg_def.smooth = 0;
+cfg_def.gausswin_size = 1; % in seconds; only used if cfg.smooth = 1
+cfg_def.gausswin_sd = 0.02; % in seconds
+cfg_def.tvec_edges = [];
 
-ProcessConfig;
+cfg = ProcessConfig2(cfg_def,cfg_in);
 mfun = mfilename;
 
 % assemble tvecs
@@ -26,16 +41,23 @@ end
 
 tvec_centers = cfg.tvec_edges(1:end-1)+cfg.dt/2;
 
-switch cfg.mode
-    case 'spikeCount'
-        for iC = length(S.t):-1:1
-            
-            spk_t = S.t{iC};
-            Q(iC,:) = trim_histc(histc(spk_t,cfg.tvec_edges));
-            
-        end
-    case 'firingRate'
-        error('firingRate mode not yet implemented.');
+% if smoothing, set up kernel
+if cfg.smooth
+    gauss_window = cfg_def.gausswin_size./cfg.dt; % 1 second window
+    gauss_SD = cfg_def.gausswin_sd./cfg.dt; % 0.02 seconds (20ms) SD
+    gk = gausskernel(gauss_window,gauss_SD); gk = gk./cfg.dt; % normalize by binsize
+end
+
+% construct Q-matrix
+for iC = length(S.t):-1:1
+    
+    spk_t = S.t{iC};
+    Q(iC,:) = trim_histc(histc(spk_t,cfg.tvec_edges));
+    
+    if cfg.smooth
+        Q(iC,:) = conv2(Q(iC,:),gk,'same'); % convolve with gaussian window
+    end
+    
 end
 
 Q = tsd(tvec_centers,Q);
