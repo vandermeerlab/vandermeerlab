b345f5ffca8f351e85622784707cda374c0dfc08
mvdm <mvdm at uwaterloo>
2015-02-01 13:31:19 -0500

    initial version of AddNActiveCellsIV()
    computes the number of active cells in iv
    *note: uses binning method for speed, i.e. is not exact
    *note: adding usr fields needs to be standardized globally (see #21)

---
+++ code-matlab/shared/datatypes/iv/AddNActiveCellsIV.m
@@ -0,0 +1,50 @@
+function iv_in = AddNActiveCellsIV(cfg_in,iv_in,S)
+% function iv_out = AddNActiveCellsIV(cfg_in,iv_in,S)
+%
+%   INPUT:
+%       cfg_in - input cfg parameters
+%       iv_in - input iv
+%       S - spikes
+%
+%   OUTPUT:
+%       iv_out - iv with nActiveCells field added
+%
+%   CFG OPTIONS:
+%
+%       cfg_def.dt = 0.001; % bin size for binning spikes
+%
+% mvdm 2015-02-01 initial version
+% note: uses binning method for speed, i.e. is not exact
+% note: adding usr fields needs to be standardized globally
+
+% parse cfg parameters
+cfg_def = [];
+cfg_def.dt = 0.005;
+
+cfg = ProcessConfig2(cfg_def,cfg_in);
+mfun = mfilename;
+
+% make matrix of spike counts (nCells x nTimeBins) 
+Q = MakeQfromS(cfg,S);
+
+% get number of active cells for each
+for iIV = length(iv_in.tstart):-1:1
+    
+    Qr = restrict(Q,iv_in.tstart(iIV),iv_in.tend(iIV));
+    
+    if ~isempty(Qr.tvec)
+        spk_counts = sum(Qr.data,2);
+        nC(iIV) = sum(spk_counts > 0);
+    else % no spikes in this iv
+        nC(iIV) = 0;
+    end
+    
+end
+
+% NOTE adding usr fields to ivs needs to be handled better
+iv_in.usr = nC;
+iv_in.label = {'nActiveCells'};
+
+% housekeeping
+iv_in.cfg.history.mfun = cat(1,iv_in.cfg.history.mfun,mfun);
+iv_in.cfg.history.cfg = cat(1,iv_in.cfg.history.cfg,{cfg});
