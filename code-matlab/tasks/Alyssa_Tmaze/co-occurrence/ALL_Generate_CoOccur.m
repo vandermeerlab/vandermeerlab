dbb75b89f33045f900f928d471fd3b1bf38f4790
aacarey <carey.alyssa@gmail.com>
2015-05-24 16:46:58 -0400

    minor change to ALL_Generate_CoOccur

--- code-matlab/tasks/Alyssa_Tmaze/co-occurrence/ALL_Generate_CoOccur.m
+++ code-matlab/tasks/Alyssa_Tmaze/co-occurrence/ALL_Generate_CoOccur.m
@@ -7,6 +7,7 @@ cfg.ExpKeysFields = {'prerecord','postrecord','goodTheta','pathlength'};
 cfg.requireMetadata = 1;
 cfg.MetadataFields = {'coord','taskvars'};
 cfg.requireCandidates = 1;
+cfg.requireVT = 1;
 cfg.requireTimes = 1; %R042 only
 cfg.requireFiles = 1;
 proceed = checkTmazeReqs(cfg); % make sure we have everything
