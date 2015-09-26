64255737e4423c03fa1bcf576f1fdb549df424ed
aacarey <carey.alyssa@gmail.com>
2015-09-26 16:46:02 -0400

    easy Tmaze color schemes with TmazeColors
    for plotting the same colors in different scripts. easy is a relative
    term. this function is intended to make switching schemes easy, and does
    not require copy-pasting rgb values for 2 conditions for 4 rats across
    different scripts when minds are changed. rather, add a new colormode
    and the new rgb values inside of the function.

---
+++ code-matlab/tasks/Alyssa_Tmaze/TmazeColors.m
@@ -0,0 +1,91 @@
+function col = TmazeColors(colormode)
+% col = TMAZECOLORS(colormode)
+% Generates a struct with rgb values for plotting Tmaze figures. This is a
+% way of easily saving and trying out different colormodes.
+%
+% output has the following organization
+%       col.all.f = [r g b]; % or 'r' depending on the mode
+%       col.R042.f = [r g b]; 
+%          ~~~
+%       col.R064.w = [r g b];
+%
+%       f is food color, w is water color
+%       f color is always darker than w color
+%
+% colormode = 
+%    'inventory1'; as in Tmaze data inventory, with combined rats in grey
+%    'inventory2'; as in inventory, with combined rats in red and blue
+%    'rb'; food is always red and water is always blue, regardless of rat
+%    'grey'; food is dark grey and water is light grey
+%
+% aacarey Sept 2015
+
+%% Example usage with dynamic struct fields during plotting:
+% 
+% rats = {'R042','R044','R050','R064'};
+% for iRat = 1:length(rats)
+%    colors = TmazeColors('inventory2');
+%    col = {colors.(rats{iRat}).f colors.(rats{iRat}).f colors.(rats{iRat}).w colors.(rats{iRat}).w}; % {foodColor foodColor waterColor waterColor}
+%    d = [val1 val2 val3 val4]; % bar graph data values
+%    %*** plot stuff here **
+%    for iBar = 1:length(d)
+%        h(iBar) = bar(location(iBar),d(iBar),'FaceColor',col{iBar},'EdgeColor','none');
+%        hold on
+%    end
+% end
+
+switch colormode
+    case 'inventory1' % with grey for combined data
+        
+        col.R042.f = [0 100/255 0];  %dark green
+        col.R042.w = [143/255 188/255 143/255];  % lighter green
+        col.R044.f = [54/255 100/255 139/255];  %dark blue
+        col.R044.w = [176/255 196/255 222/255];  % lighter blue
+        col.R050.f = [104/255 34/255 139/255];  %dark purple
+        col.R050.w = [216/255 191/255 216/255];  % lighter purple
+        col.R064.f = [198/255 113/255 113/255];  %dark salmon
+        col.R064.w = [238/255 213/255 210/255];  % lighter salmon/dusty pink
+        col.all.f =  [105/255 105/255 105/255];  %dark grey
+        col.all.w =  [211/255 211/255 211/255];  % lighter grey
+        
+    case 'inventory2' % with rb for combined data
+        
+        col.R042.f = [0 100/255 0];  %dark green
+        col.R042.w = [143/255 188/255 143/255];  % lighter green
+        col.R044.f = [54/255 100/255 139/255];  %dark blue
+        col.R044.w = [176/255 196/255 222/255];  % lighter blue
+        col.R050.f = [104/255 34/255 139/255];  %dark purple
+        col.R050.w = [216/255 191/255 216/255];  % lighter purple
+        col.R064.f = [198/255 113/255 113/255];  %dark salmon
+        col.R064.w = [238/255 213/255 210/255];  % lighter salmon/dusty pink
+        col.all.f =  'r';
+        col.all.w =  'b';
+        
+    case 'rb'
+        col.R042.f = 'r';
+        col.R042.w = 'b';
+        col.R044.f = 'r';
+        col.R044.w = 'b';
+        col.R050.f = 'r';
+        col.R050.w = 'b';
+        col.R064.f = 'r';
+        col.R064.w = 'b';
+        col.all.f =  'r';
+        col.all.w =  'b';
+        
+    case 'grey'
+        col.R042.f = [105/255 105/255 105/255];  %dark grey
+        col.R042.w = [211/255 211/255 211/255];  % lighter grey
+        col.R044.f = [105/255 105/255 105/255]; 
+        col.R044.w = [211/255 211/255 211/255]; 
+        col.R050.f = [105/255 105/255 105/255];  
+        col.R050.w = [211/255 211/255 211/255]; 
+        col.R064.f = [105/255 105/255 105/255]; 
+        col.R064.w = [211/255 211/255 211/255]; 
+        col.all.f =  [105/255 105/255 105/255];  
+        col.all.w =  [211/255 211/255 211/255];  
+        
+    otherwise
+        error('Unrecognized colormode. Better check that spelling ^_^')
+end
+
