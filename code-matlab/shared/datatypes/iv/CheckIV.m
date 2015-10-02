918d003bbc0b46d5567d431a66de68de4ecd5ae9
aacarey <carey.alyssa@gmail.com>
2015-10-02 17:11:42 -0400

    added more checks to CheckIV
    now it assumes that a user might put just about anything in as an "iv"
    datatype, and prints corresponding instructions about why the input was
    rejected

--- code-matlab/shared/datatypes/iv/CheckIV.m
+++ code-matlab/shared/datatypes/iv/CheckIV.m
@@ -10,10 +10,23 @@ function pass_flag = CheckIV(iv_in)
 % pass_flag: 1 if all checks pass, 0 if otherwise
 %
 % MvdM 2014-11-12
+% aacarey edit Sept 2015, additional checks
 
 pass_flag = 1;
 
-if ~iscolumn(iv_in.tstart) || ~iscolumn(iv_in.tend)
-   pass_flag = 0;
-   fprintf('FAIL: t_start and/or t_end are not column vectors.\n');
+if isstruct(iv_in)
+    if ~isfield(iv_in.tstart,'tstart') || ~isfield(iv_in.tend,'tend')
+        pass_flag = 0;
+        fprintf('FAIL: input iv must contain tstart and tend fields.\n');
+    end
+    
+    if ~iscolumn(iv_in.tstart) || ~iscolumn(iv_in.tend)
+        pass_flag = 0;
+        fprintf('FAIL: tstart and tend must be column vectors.\n');
+    end
+else
+    pass_flag = 0;
+    fprintf('FAIL: input must be an iv data type with tstart and tend fields.\n');
 end
+   
+
