7ac2ce30d09da6d0209ded384683cb951a4837a0
aacarey <carey.alyssa@gmail.com>
2015-11-17 18:08:50 -0500

    some datatype constructor changes (please contribute?)
    - tried adding some help stuff to the dataype functions (can be better)

--- code-matlab/shared/datatypes/iv/CheckIV.m
+++ code-matlab/shared/datatypes/iv/CheckIV.m
@@ -1,10 +1,14 @@
-function pass_flag = CheckIV(iv_in)
+function pass_flag = CheckIV(iv_in,varargin)
 % function pass_flag = CheckIV(iv_in)
 %
 % checks if iv is well formed
 %
 % INPUTS:
 % iv_in: iv to be checked
+% 
+% varargins:
+%  callername: name of invoking function (for more informative warning
+%  messages)
 %
 % OUTPUTS:
 % pass_flag: 1 if all checks pass, 0 if otherwise
@@ -14,19 +18,22 @@ function pass_flag = CheckIV(iv_in)
 
 pass_flag = 1;
 
+in_mfun = '';
+if ~isempty(varargin) && ischar(varargin{1})
+    in_mfun = [' in ',varargin{1}];
+end
+
 if isstruct(iv_in)
     if ~isfield(iv_in,'tstart') || ~isfield(iv_in,'tend')
         pass_flag = 0;
-        fprintf('FAIL: input iv must contain tstart and tend fields.\n');
-    end
-    
-    if ~iscolumn(iv_in.tstart) || ~iscolumn(iv_in.tend)
+        fprintf('FAIL%s: input iv must contain tstart and tend fields.\n',in_mfun);
+    elseif ~iscolumn(iv_in.tstart) || ~iscolumn(iv_in.tend)
         pass_flag = 0;
-        fprintf('FAIL: tstart and tend must be column vectors.\n');
+        fprintf('FAIL%s: tstart and tend must be column vectors.\n',in_mfun);
     end
 else
     pass_flag = 0;
-    fprintf('FAIL: input must be an iv data type with tstart and tend fields.\n');
+    fprintf('FAIL%s: input must be an iv data type.\n',in_mfun);
 end
    
 
--- code-matlab/shared/datatypes/tsd/tsd.m
+++ code-matlab/shared/datatypes/tsd/tsd.m
@@ -1,13 +1,35 @@
 function tsd_out = tsd(varargin)
-% function tsd_out = tsd(varargin)
+% TSD Time-stamped data datatype constructor
+%   Time-stamped data, or tsd, is one of the main data types. Each tsd
+%   struct contains a time vector and corresponding data (such as voltages 
+%   or position). 
 %
-% constructor for tsd (timestamped data) struct
+%   function tsd_out = TSD(tvec,data)
 %
-% tsd_out = tsd(tvec,data);
-% tsd_out = tsd(tvec,data,'label');
+%   function tsd_out = TSD(tvec,data,'label')
+%
+%   INPUTS:
+%      tvec: time vector (time stamps in ascending order)
+%      data: measurements taken at each time stamp
+%     label: unique identifier
+%
+%   OUTPUTS
+%      iv_out: iv struct with fields:
+%           .type   - 'tsd'; datatype identification
+%           .tvec   - [nx1] double, where n is the number of time samples taken
+%           .data   - [1xn] double containing measures taken at each sample
+%                     point
+%           .label  - Unique identifier for the data contained with the tsd,
+%                     such as a record of the data source (in the case of a 
+%                     CSC, the label corresponds to the tetrode that recorded 
+%                     the signal)
+%           .cfg    - record of the data's history including config 
+%                     parameters and functions visited
 %
 % MvdM 2014-06-17
+% aacarey edit, Nov 2015
 
+tsd_out.type = 'tsd';
 tsd_out.tvec = [];
 tsd_out.data = [];
 tsd_out.label = {};
