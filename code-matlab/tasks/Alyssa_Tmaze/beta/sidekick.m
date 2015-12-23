423e6a0ebb79c13386086e508443b940b8ce15a4
aacarey <carey.alyssa@gmail.com>
2015-12-23 14:35:39 -0500

    minor changes to sidekick

--- code-matlab/tasks/Alyssa_Tmaze/beta/sidekick.m
+++ code-matlab/tasks/Alyssa_Tmaze/beta/sidekick.m
@@ -23,9 +23,11 @@ function h = sidekick(cfg_in,varargin)
 %
 %   CFG OPTIONS
 %       cfg.LineWidth = 1.5; 
+%       cfg.showLegend = 1; If 1 makes legend, if 0 doesn't. Useful if
+%              sidekick is called inside of another function and you don't 
+%              want a legend and don't want to deal with deleting it.
 %       cfg.Location = 'NorthEast'; Location of legend.
 %       cfg.ColVal   = [nVararg x 3] array of [r g b] values
-%   
 %
 % RESCALING NOT IMPLEMENTED YET
 % aacarey, Jan 2015, Nov 2015
@@ -35,19 +37,21 @@ function h = sidekick(cfg_in,varargin)
 cfg_def.LineWidth = 1.5;
 cfg_def.ColVal = linspecer(length(varargin)); % get a bunch of colors to use
 cfg_def.Location = 'NorthEast';
-cfg_def.openNewFig = 0;
-cfg = ProcessConfig2(cfg_def,cfg_in);
+cfg_def.showLegend = 1;
 
-if cfg.openNewFig
-    figure;
-end
+mfun = mfilename;
+
+cfg = ProcessConfig(cfg_def,cfg_in,mfun);
+
 hold on;
 lgnd = cell(size(varargin));
 h = zeros(1,length(varargin)+1);
 
 for iArg = 1:length(varargin)
     temp = varargin{iArg};
-    name = inputname(iArg+1); if isempty(name); name = ['Input ',num2str(iArg)]; end; lgnd{iArg} = name;
+    if cfg.showLegend
+        name = inputname(iArg+1); if isempty(name); name = ['Input ',num2str(iArg)]; end; lgnd{iArg} = name;
+    end
     
     % check if Y is an iv data type
     if isfield(temp,'tstart')
@@ -66,8 +70,6 @@ for iArg = 1:length(varargin)
 end
 
 % now do legend
-h(iArg+1) = legend(h(1:iArg),lgnd);
+if cfg.showLegend; h(iArg+1) = legend(h(1:iArg),lgnd); set(h(iArg+1),'Location',cfg.Location); end
 
-set(h(iArg+1),'Location',cfg.Location)
-
-hold off;
+end
