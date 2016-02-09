f0d7c6a3bc8548b7d4fd264c17c92bb3b52d7e8e
mvdm <mvdm@uwaterloo.ca>
2016-02-09 16:30:07 -0500

    fixed ft_maketrl()

--- code-matlab/shared/ft/ft_maketrl.m
+++ code-matlab/shared/ft/ft_maketrl.m
@@ -6,7 +6,7 @@ function trl = ft_maketrl(cfg_in)
 % cfg.t: trial center times (zero or trigger points) in seconds
 % cfg.twin: [pre post] times in s to define start and end, e.g. [-2 2] in
 % seconds
-% cfg.mode: 'ft' for fieldtrip-based input times (default) or 'nlx; for
+% cfg.mode: 'ft' for fieldtrip-based input times (default) or 'nlx' for
 % neuralynx-based
 % cfg.hdr: hdr of ft data structure you want indices for
 %
@@ -37,9 +37,9 @@ end
 % construct tvec based on mode
 if strcmp(cfg.mode,'nlx')
     % times are on Neuralynx timebase, subtract time of first sample
-    cfg.t = cfg.t - double(cfg.hdr.FirstTimeStamp);
-    tvec = cfg.tvec-cfg.hdr.FirstTimeStamp; %minus first time to align to zero
-    
+    cfg.t = cfg.t - double(cfg.hdr.FirstTimeStamp)/10^6;
+    %tvec = (cfg.hdr.FirstTimeStamp:cfg.hdr.TimeStampPerSample:cfg.hdr.LastTimeStamp)-double(cfg.hdr.FirstTimeStamp); %minus first time to align to zero
+    tvec = cat(2,0,cumsum(repmat(1./cfg.hdr.Fs,[1 cfg.hdr.nSamples-1])));
 else %equivalent to elseif cfg.mode = 'ft'
     tvec = cat(2,0,cumsum(repmat(1./cfg.hdr.Fs,[1 cfg.hdr.nSamples-1])));
 end
