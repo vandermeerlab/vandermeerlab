27c31b45f9348c74cb2cafe302394c476b2074d8
aacarey <carey.alyssa@gmail.com>
2016-02-17 15:01:43 -0500

    switched DifferenceIV over to ProcessConfig

--- code-matlab/shared/datatypes/iv/DifferenceIV.m
+++ code-matlab/shared/datatypes/iv/DifferenceIV.m
@@ -1,7 +1,12 @@
-function iv1 = DifferenceIV(cfg_in,iv1,iv2)
-% function iv = DifferenceIV(cfg,iv1,iv2)
+function iv_out = DifferenceIV(cfg_in,iv1,iv2)
+% function iv_out = DifferenceIV(cfg,iv1,iv2)
 %
 % keep only those iv1's that DO NOT include a piece of iv2
+%
+%  iv1    ____________    _____  ______     ____________   ______
+%  iv2        ________    _____                _______             ______
+%
+%  iv_out                        ______                    ______
 %
 % INPUTS:
 %
@@ -9,6 +14,7 @@ function iv1 = DifferenceIV(cfg_in,iv1,iv2)
 % iv2: interval data to be used to select from iv1
 %
 % CFG OPTIONS:
+% cfg.verbose = 1; If 1, displays text in command window; if 0, doesn't.
 %
 % OUTPUTS:
 %
@@ -16,10 +22,12 @@ function iv1 = DifferenceIV(cfg_in,iv1,iv2)
 %
 % MvdM 2014-06-28
 
-cfg_def = [];
-
-cfg = ProcessConfig2(cfg_def,cfg_in); % should take whatever is in cfg_in and put it into cfg!
+cfg_def.verbose = 1;
 mfun = mfilename;
+
+cfg = ProcessConfig(cfg_def,cfg_in,mfun); % should take whatever is in cfg_in and put it into cfg!
+
+if ~CheckIV(iv1) || ~CheckIV(iv2); error('Inputs must be iv data type'); end
 
 keep = ones(length(iv1.tstart),1);
 for iI = 1:length(iv1.tstart)
@@ -43,8 +51,11 @@ for iI = 1:length(iv1.tstart)
     
 end
 
-iv1.tstart = iv1.tstart(logical(keep));
-iv1.tend = iv1.tend(logical(keep));
+iv_out = iv1;
+iv_out.tstart = iv_out.tstart(logical(keep));
+iv_out.tend = iv_out.tend(logical(keep));
+
+if cfg.verbose; fprintf('%s: %d intervals in, %d intervals out\n',mfun,length(iv1.tstart) + length(iv2.tstart),length(iv_out.tstart)); end
 
 % housekeeping
 iv1.cfg.history.mfun = cat(1,iv1.cfg.history.mfun,mfun);
