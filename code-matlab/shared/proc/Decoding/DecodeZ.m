1aec108a940ba0e9910e0e271c55a10ad6d41342
mvdm <mvdm@uwaterloo.ca>
2016-05-19 16:24:34 -0400

    Implemented multiple ways to exclude time bins in DecodeZ()
    Can now choose to do so by requiring a certain minimum number of neurons to exceed a firing threshold, or by a population average

--- code-matlab/shared/proc/Decoding/DecodeZ.m
+++ code-matlab/shared/proc/Decoding/DecodeZ.m
@@ -18,9 +18,9 @@ function p_tsd = DecodeZ(cfg_in,Q,tc)
 %
 % cfg.noSpikesInBin = 'zeros'; % {'zeros','nans'}, what to put in time bins
 %   below threshold
-% cfg_def.nMinSpikes = 1; % minimum number of spikes (sum) for bin to be
-%   included -- note this gets hard to interpret when smoothing; could
-%   normalize to firing rate?
+% cfg_def.excludeMethod = 'frate'; % 'frate', 'nNeurons'; criterion for excluding time bin
+% cfg_def.nMinNeurons = 1; % if cfg.excludeMethod = 'nNeurons', specify how many neurons need to be active
+% cfg_def.nMinSpikes = 1; % threshold for counting neuron as active (if cfg.excludeMethod = 'nNeurons') or bin as active (if cfg_def.excludeMethod = 'frate')
 %
 % NOTE: assumes Q.data is in format [nCells x nTimeBins]
 %
@@ -29,7 +29,9 @@ function p_tsd = DecodeZ(cfg_in,Q,tc)
 
 cfg_def = [];
 cfg_def.noSpikesInBin = 'nans';
-cfg_def.nMinSpikes = 1;
+cfg_def.excludeMethod = 'frate'; % 'frate', 'nNeurons'; criterion for excluding time bin
+cfg_def.nMinNeurons = 1; % if cfg.excludeMethod = 'nNeurons', specify how many neurons need to be active
+cfg_def.nMinSpikes = 1; % threshold for counting neuron as active (if cfg.excludeMethod = 'nNeurons') or bin as active (if cfg_def.excludeMethod = 'frate')
 
 cfg = ProcessConfig(cfg_def,cfg_in);
 mfun = mfilename;
@@ -52,13 +54,20 @@ end
 p = p./repmat(nansum(p,2),1,nBins); % renormalize
 
 % deal with bins without any spikes
-nActiveNeurons = sum(Q.data);
+switch cfg.excludeMethod
+    case 'nNeurons' % number of neurons with at least this activity level
+        nActiveNeurons = sum(Q.data >= cfg.nMinSpikes);
+        toss_idx = nActiveNeurons < cfg.nMinNeurons;
+    case 'frate'
+        nActiveNeurons = sum(Q.data); % this is an overall activity level; not how many individual neurons pass an activity threshold
+        toss_idx = nActiveNeurons < cfg.nMinSpikes;
+end
 
 switch cfg.noSpikesInBin
     case 'zeros'
-        p(nActiveNeurons < cfg.nMinSpikes,:) = 0;
+        p(toss_idx,:) = 0;
     case 'nans'
-        p(nActiveNeurons < cfg.nMinSpikes,:) = NaN;
+        p(toss_idx,:) = NaN;
 end
 
 %
