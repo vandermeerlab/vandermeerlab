787cd23760e649428636bf5e51271bfde8fac293
youkitan <youkitan@gmail.com>
2016-12-10 22:09:39 -0500

    fixes to comments
    - updated CheckTC for tc constructor
    - included a bunch of checks so tccmp does what its supposed to (could
    implement more checks)
    - genInhomogen.. now has cfg options

--- code-matlab/shared/datatypes/tc/CheckTC.m
+++ code-matlab/shared/datatypes/tc/CheckTC.m
@@ -12,11 +12,16 @@ function pass_flag = CheckTC(tc_in,varargin)
 %       pass_flag: 1 if all checks pass, 0 if otherwise
 %
 %      Checks performed:
-%        -  .tc field must exist (FAIL)
-%        -  binsize must agree across variables (FAIL)
+%        -  .tc or .tc2D field must exist (FAIL)
+%        -  .tc must be an nCells x nBins matrix (FAIL)
+%        -  .tc2D must be an nCells x nXBins x nYBins matrix (FAIL)
+%        -  nBins must agree across variables (FAIL)
 %        -  number of cells must agree across variables (FAIL)
 %
+%       see also: tc, TuningCurves
+%
 % youkitan 2016-11-29 initial
+% youkitan 2016-12-08 edits added some tc2D functionality
 
 pass_flag = 1;
 
@@ -29,20 +34,38 @@ if ~isstruct(tc_in)
     pass_flag = 0;
     fprintf('FAIL%s: input must be a tc struct.\n',in_mfun);
 else
-    % checks for 1D case
-    if isfield(tc_in,'tc')
-        if size(tc_in.tc,2) ~= size(tc_in.occ_hist,2)
+    % basic checks
+    if isfield(tc_in,'type')
+        if ~isequal(tc_in.type,'tc')
+            fprintf('FAIL%s: input must be a tc type.\n',in_mfun);
             pass_flag = 0;
-            fprintf('FAIL%s: number of bins differ between tc and occ_hist.\n',in_mfun);
+            
+        % checks for 1D case
+        elseif isfield(tc_in,'tc')
+            if length(size(tc_in.tc)) ~= 2
+                fprintf('FAIL%s: tc variable should be an nCells x nBins matrix.\n',in_mfun);
+                pass_flag = 0;
+
+            elseif size(tc_in.tc,2) ~= size(tc_in.occ_hist,2)
+                pass_flag = 0;
+                fprintf('FAIL%s: number of bins differ between tc and occ_hist.\n',in_mfun);
+            end
+
+        % checks for 2D case    
+        elseif isfield(tc_in,'tc2D')
+            if length(size(tc_in.tc2D)) ~= 3
+                fprintf('FAIL%s: tc variable should be an nCells x nXBins x nYBins matrix.\n',in_mfun);
+                pass_flag = 0;
+            end
+            fprintf('WARNING: Checks for 2D tuning curves not implemented yet!!!')
+
+        else %no tuning variable
+            pass_flag = 0;
+            fprintf('FAIL%s: input tc must contain a tc field.\n',in_mfun);
         end
-        
-    % checks for 2D case    
-    elseif isfield(tc_in,'tc2D')
-        fprintf('WARNING: Checks for 2D tuning curves not implemented yet!!!')
-    
-    else %no tuning variable
-        pass_flag = 0;
-        fprintf('FAIL%s: input tc must contain a tc field.\n',in_mfun);
+    else
+       fprintf('FAIL%s: input has no type parameter.\n',in_mfun)
+       pass_flag = 0;
     end
    
 end
--- code-matlab/shared/util/genInhomogeneousPoisson.m
+++ code-matlab/shared/util/genInhomogeneousPoisson.m
@@ -1,23 +1,38 @@
-function [spiketrain] = genInhomogeneousPoisson(max_rate, total_time, ratefunc, nTrials)
-% function [spiketrain] = genInhomogeneousPoisson(max_rate, total_time, ratefunc);
-%  Generate an inhomogenous Poisson process spike train using deletion
-%   max_rate = maximum instantaneous firing rate
-%   total_time = length of time to simulate for (starts at time 0)
-%   ratefunc = function which should return rate at time t (should not exceed max_rate)
+function spiketrain = genInhomogeneousPoisson(cfg_in,max_rate, total_time, ratefunc)
+%% GENINHOMOGENEOUSPOISSON Generate Inhomogeneous Poisson spikes 
+% [spiketrain] = genInhomogeneousPoisson(max_rate, total_time, ratefunc) Generates an
+% inhomogenous Poisson process spike train using a deletion method
+%
+%   INPUT:
+%       max_rate - maximum instantaneous firing rate
+%       total_time - length of time to simulate for (starts at time 0)
+%       ratefunc - function which should return rate at time t (should not exceed max_rate)
+%
+%   OUTPUT:
+%       S_out - output ts object
+%
+%   CFG OPTIONS:
+%       cfg.nTrials = 1; Number of trials.
+%       cfg.rp = .001; Refractory period in seconds.
+%       cfg.verbose = 0; If 1, print informative text to command window; if 0, be silent.
+%
+% youkitan 2016-12-08 edits: cfg parameters
 
-% Generate inhomogeneous Poisson process
+%% parse cfg parameters
+cfg_def = [];
+cfg_def.nTrials = 1;
+cfg_def.refractoryPeriod = .001;
 
-if nargin < 3
-  error('Must define rate function.');
-elseif nargin < 4
-  nTrials = 1;
-end
+mfun = mfilename;
+cfg = ProcessConfig(cfg_def,cfg_in,mfun);
 
-Homogeneous_Spikes = poissrnd( (max_rate * total_time) * ones(nTrials,1) ); %number of spikes
+%% main body
+
+Homogeneous_Spikes = poissrnd( (max_rate * total_time) * ones(cfg.nTrials,1) ); %number of spikes
 % beta = 1.2;
 % Homogeneous_Spikes = round(gamrnd((max_rate * total_time)/beta, beta) * ones(nTrials,1) );
 
-for k = 1:nTrials
+for k = 1:cfg.nTrials
     Homogeneous_Spike_Times = rand(Homogeneous_Spikes(k),1) * total_time; %convert to spiketimes
     spikes = sort(Homogeneous_Spike_Times);
     spikes = renewal(spikes); % enforced absolute refractory period of 1ms
@@ -30,13 +45,12 @@ for k = 1:nTrials
         spikes = zeros(0,1);
     end
     spiketrain{k} = spikes;
-%     spiketrain{k} = length(spikes);
 end
 
 end
 
 function spikes_out = renewal(spikes_in)
-    del_idx = find(diff(spikes_in) < 0.001); %1ms refractory
+    del_idx = find(diff(spikes_in) < cfg.refractoryPeriod); %1ms refractory
     if ~isempty(del_idx)
         del_idx = del_idx + 1; %this is actual index of successive spike time
         spikes_in(del_idx) = [];
