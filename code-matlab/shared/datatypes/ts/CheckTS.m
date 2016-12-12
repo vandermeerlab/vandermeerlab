70a79ba09763ceabdb2f47ffde12cf085ccab6db
youkitan <youkitan@gmail.com>
2016-12-11 21:03:05 -0500

    Increased readability for all datatype check functions
    - standardized check function help
    - checks performed
    - see also (datatype constructor, other checks)
    - included caller input for CheckTSD
    - added name of check for displayed output

--- code-matlab/shared/datatypes/ts/CheckTS.m
+++ code-matlab/shared/datatypes/ts/CheckTS.m
@@ -1,25 +1,27 @@
 function pass_flag = CheckTS(ts_in,varargin)
-%CHECKTS Verify that input is ts and is well formed
+%% CHECKTS Check TS for datatype violations
+%	pass_flag = CheckTS(ts_in,varargin) verifies that input is ts and is well formed.
 %
-%    INPUTS:
+%	INPUTS:
 %       ts_in: ts to be checked
-% 
 %       varargins:
-%       callername: name of invoking function (for more informative warning
-%       messages)
+%           callername: name of invoking function (for more informative warning messages)
 %
-%       OUTPUTS:
+%	OUTPUTS:
 %       pass_flag: 1 if all checks pass, 0 if otherwise
 %
-%      Checks performed:
-%        -  .t field must exist (FAIL)
-%        -  all contents of .t must be column vectors (FAIL)
-%        -  is there a .label field? (WARNING)
-%        -  is .label the same length as .t? (FAIL)
-%        -  is there a .type field? (WARNING)
-%        -  does the .type field correctly identify as ts? (WARNING)
+%	Checks performed:
+%       -  .t field must exist (FAIL)
+%       -  all contents of .t must be column vectors (FAIL)
+%       -  is there a .label field? (WARNING)
+%       -  is .label the same length as .t? (FAIL)
+%       -  is there a .type field? (WARNING)
+%       -  does the .type field correctly identify as ts? (WARNING)
+%
+%   see also ts, CheckTSD, CheckIV, CheckTC
 %
 % aacarey Nov 2015
+% youkitan edit Dec 2016, reformat help, add function name to output
 
 pass_flag = 1;
 
@@ -30,19 +32,19 @@ end
 
 if ~isstruct(ts_in)
     pass_flag = 0;
-    fprintf('FAIL%s: input must be a ts datatype.\n',in_mfun);
+    fprintf('FAIL%s by CheckTS: input must be a ts datatype.\n',in_mfun);
 else
     % check for .t field
     if ~isfield(ts_in,'t')
         pass_flag = 0;
-        fprintf('FAIL%s: input ts must contain t field.\n',in_mfun);
+        fprintf('FAIL%s by CheckTS: input ts must contain t field.\n',in_mfun);
         
     else
         % check that all t are column vectors
         for iT = 1:length(ts_in.t)
             if ~iscolumn(ts_in.t{iT})
                 pass_flag = 0;
-                fprintf('FAIL%s: all contents of ts_in.t must be column vectors.\n',in_mfun);
+                fprintf('FAIL%s by CheckTS: all contents of ts_in.t must be column vectors.\n',in_mfun);
             end
         end
     end
@@ -50,25 +52,24 @@ else
     % check for label field
     if ~isfield(ts_in,'label')
         %pass_flag = 0;
-        fprintf('WARNING%s: ts_in lacks a .label field.\n',in_mfun);
+        fprintf('WARNING%s by CheckTS: ts_in lacks a .label field.\n',in_mfun);
     else
         % check that label is the same length as t
         if length(ts_in.label) ~= length(ts_in.t)
             pass_flag = 0;
-            fprintf('FAIL%s: ts_in.t and ts_in.label must have the same length.\n',in_mfun);
+            fprintf('FAIL%s by CheckTS: ts_in.t and ts_in.label must have the same length.\n',in_mfun);
         end
     end
 
     % check for type field
     if pass_flag && ~isfield(ts_in,'type')
         %pass_flag = 0;
-        fprintf('WARNING%s: input appears to be ts datatype but lacks the .type field.\n',in_mfun);
+        fprintf('WARNING%s by CheckTS: input appears to be ts datatype but lacks the .type field.\n',in_mfun);
     elseif pass_flag && isfield(ts_in,'type') && ~strcmp(ts_in.type,'ts')
         %pass_flag = 0;
-        fprintf('WARNING%s: input appears to be ts datatype but is identified as %s in the .type field.\n',in_mfun,ts_in.type);
+        fprintf('WARNING%s by CheckTS: input appears to be ts datatype but is identified as %s in the .type field.\n',in_mfun,ts_in.type);
     end
     
 end
 
 end
-
