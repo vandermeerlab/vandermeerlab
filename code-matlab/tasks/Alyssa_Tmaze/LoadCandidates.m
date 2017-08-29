1ef50e0c3462022ed204cd8cc49ee99201f13edf
mvdm <mvdm@uwaterloo.ca>
2017-08-29 11:41:41 -0400

    Added suffix cfg option to LoadCandidates()

--- code-matlab/tasks/Alyssa_Tmaze/LoadCandidates.m
+++ code-matlab/tasks/Alyssa_Tmaze/LoadCandidates.m
@@ -1,4 +1,4 @@
-function LoadCandidates
+function LoadCandidates(varargin)
 %LOADCANDIDATES Load candidates into caller workspace
 %   No need for any inputs; it checks the current directory automatically
 %
@@ -9,16 +9,30 @@ function LoadCandidates
 % found in the current directory or subdirectories.
 %
 % A.Carey May 2015 
+% MvdM 2017 modified to accept cfg.suffix input; implemented as varargin for
+% compatibility
 
-%%
+suffix = [];
+switch nargin
+    case 0
+    case 1
+        if isfield(varargin{1},'suffix')
+            suffix = varargin{1}.suffix;
+        else
+            error('I couldn''t find any input fields I know how to handle.');
+        end
+    otherwise
+        error('I don''t know what to do with more than one input argument.');
+end
 
 %[~,name,~] = fileparts(pwd); 
 
-fn = FindFiles('*-candidates.mat');
+fstring = cat(2,'*-candidates',suffix,'.mat');
+fn = FindFiles(fstring);
 if isempty(fn)
-    disp(['LoadCandidates: No files matching',' ''*candidates.mat'' ','were found in', [' ',pwd]])
+    fprintf('LoadCandidates: no files matching %s were found in %s\n',fstring,pwd);
 elseif length(fn) > 1
-    disp(['LoadCandidates: More than one file matching',' ''*candidates.mat'' ','was found in', [' ',pwd]])
+    fprintf('LoadCandidates: more than one file matching %s was found in %s\n',fstring,pwd);
     disp('candidates not loaded')
 else
     load(fn{1})
