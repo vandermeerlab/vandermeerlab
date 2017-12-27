1336d32c41f046158552b8b9c47e9810bcf275a2
aacarey <carey.alyssa@gmail.com>
2017-12-27 10:46:46 -0500

    CommonIV speed increase

--- code-matlab/shared/datatypes/iv/CommonIV.m
+++ code-matlab/shared/datatypes/iv/CommonIV.m
@@ -27,9 +27,6 @@ function IV = CommonIV(cfg,ivA,ivB,varargin)
 %           non-overlapping.
 % 
 %   CONFIG OPTIONS
-%      cfg.binsize = 1/2000; Internal timebase (same units as the input IVs)
-%                    for deciding whether the intervals intersect. If the
-%                    binsize is too large, merging can occur.
 %      cfg.threshold = 1; What proportion of inputs need to agree on a 
 %                    timespan for it to be kept as an interval? (this is a
 %                    number between 0 and 1, such as 2/3 or 9/10)
@@ -48,8 +45,8 @@ function IV = CommonIV(cfg,ivA,ivB,varargin)
 %   see also: UnionIV2, OverlapIV
 %
 % aacarey Feb 2016
+% cfdu@uwaterloo.ca Nov 2017 implemented to remove binning process, speed 500x
 
-cfg_def.binsize = 1/2000;
 cfg_def.threshold = 1;
 cfg_def.keepGaps = 1; % if 1, preserves areas that any of the intervals think should be separate
 cfg_def.verbose = 1;
@@ -59,24 +56,17 @@ cfg = ProcessConfig(cfg_def,cfg,mfun);
 
 if cfg.verbose; tic; fprintf('%s: looking for time intersections in iv data...\n',mfun); end
 
-nArgs = 2 + length(varargin);
+args = {ivA, ivB, varargin{:}};
+nArgs = length(args);
 
 % we could do a CheckIV here, but it's redundant since MergeIV below also
 % does it, though the user won't know specifically which iv is bad...
 
 %~~~ don't allow overlap within individual input intervals ~~~
 cfg_temp = []; cfg_temp.verbose = 0; cfg_temp.gap = 0;
-ivA = MergeIV(cfg_temp,ivA);
-ivB = MergeIV(cfg_temp,ivB);
-for iVarg = 1:length(varargin)
-    varargin{iVarg} = MergeIV(cfg_temp,varargin{iVarg});
+for iArg = 1:nArgs
+    args{iArg} = MergeIV(cfg_temp,args{iArg});
 end
-
-%~~~ use UnionIV2 to collect the inputs into a single iv struct ~~~
-cfg_temp = []; cfg_temp.verbose = 0; cfg_temp.rmdoubles = 0;
-% rmdoubles is zero because if there ARE copies across ivs generated
-% differently, then we want to keep this because it's an agreement
-IV_temp = UnionIV2(cfg_temp,ivA,ivB,varargin{:});
 
 % suppose we have these intervals:
 %                ____     __               ____________________   
@@ -84,75 +74,67 @@ IV_temp = UnionIV2(cfg_temp,ivA,ivB,varargin{:});
 %       __________________________       ___________________________
 %                      <---------- tvec ---------->
 
-%~~~ find the earliest and latest entries in all the input IVs ~~~
-earliest = IV_temp.tstart(1); % starts are already sorted ascending
-latest = max(IV_temp.tend); % ends are not necessarily sorted
-
-%~~~ construct internal tvec ~~~
-tvec = earliest:cfg.binsize:latest;
-
-%~~~ bin interval agreement, this pairs with tvec so we can extract boundaries later ~~~
-commonBins = zeros(1,length(tvec));
-
-for iInterval = 1:length(IV_temp.tstart)
-    withinInterval = find(tvec >= IV_temp.tstart(iInterval) & tvec <= IV_temp.tend(iInterval));
-    commonBins(withinInterval) = commonBins(withinInterval) + 1;
-end
-
-% now we have somthing like this in commonBins, where the numbers 1-4
-% indicate how many intervals contained the time bin in question:
 %                ____     __                      _______
 % 3        _____|    |___|  |              ______|       |_____
 % 2     __|                 |_____       _|                    |____
 % 1 ___|                          |_____|                           |_____
 %                      <---------- tvec ---------->
 
-%~~~ threshold commonBins ~~~
-% convert threshold into a number compared to the total number of input ivs
-threshold = nArgs*cfg.threshold;
-commonBins(commonBins < threshold) = 0;
+num_intervals = 0;
+for iArg = 1:nArgs
+    num_intervals = num_intervals + length(args{iArg}.tstart);
+end
+hist = nan(num_intervals, 2);
+curr_index = ones(nArgs, 1);
+curr_points = nan(nArgs, 1);
+for iArg = 1:nArgs
+    curr_points(iArg) = args{iArg}.tstart(1);
+end
 
-% so now maybe we have something like this:
-%                ____     __                      _______
-% 3        _____|    |___|  |              ______|       |_____
-% 2       |                 |             |                    |
-% 1 ______|                 |_____________|                    |__________
-%                      <---------- tvec ---------->
+for hist_idx = 1:2*num_intervals
+    [pos, min_idx] = min(curr_points);
+    is_start = mod(curr_index(min_idx), 2);
+    hist(hist_idx, 1) = pos;
+    hist(hist_idx, 2) = (-1) ^ ~is_start;
+    curr_index(min_idx) = curr_index(min_idx) + 1;
+    next_arg_index = ceil(curr_index(min_idx) / 2);
+    if is_start
+        curr_points(min_idx) = args{min_idx}.tend(next_arg_index);
+    else
+        if next_arg_index <= length(args{min_idx}.tstart)
+            curr_points(min_idx) = args{min_idx}.tstart(next_arg_index);
+        else
+            curr_points(min_idx) = inf;
+        end
+    end
+end
 
-if cfg.keepGaps % find valleys and pull them down
-    % invert commonBins so that the valleys are now peaks that we can
-    % detect with findpeaks
-    
-    commonBinsInverted = commonBins * -1;
-    [~,minHere] = findpeaks(commonBinsInverted);
-    commonBins(minHere) = 0;
+hist(:, 2) = cumsum(hist(:,2));
+hist = hist(logical([diff(hist(:, 1)); size(hist, 1)]), :);
+
+
+%if cfg.keepGaps % find valleys and pull them down
 % 4              ____     __                      _______
 % 3        _____|    |   |  |              ______|       |_____
 % 2       |          |   |  |             |                    |
 % 1 ______|          |___|  |_____________|                    |__________
 %                      <---------- tvec ---------->
+if cfg.keepGaps
+    temp = diff(hist(:, 2));
+    hist([0; temp < 0] & [temp > 0; 0], 2) = 0;
 end
-
-%~~~ convert to tsd and keep everything above zero ~~~
-commonTSD = tsd(tvec,commonBins);
-cfg_temp = []; cfg_temp.verbose = 0; cfg.method = 'raw'; cfg_temp.threshold = 0; cfg_temp.operation = '>';
-IV = TSDtoIV2(cfg_temp,commonTSD);
 
 % if we wanted 2/3 to agree, then our intervals look like:
 %          __________     __               ____________________
 %                      <---------- tvec ---------->
+threshold = nArgs*cfg.threshold;
+start_idx = find(hist(:, 2) >= threshold);
+end_idx = start_idx + 1;
+IV = iv(hist(start_idx), hist(end_idx));
 
-%~~~ adjust boundaries ~~~
-% the boundaries in IV should be derived from the boundaries in IV_temp
-IV.tstart = nearval3(IV.tstart,IV_temp.tstart,0);
-% binary search requires that these are in sorted order:
-lookups = sort(IV_temp.tend);
-IV.tend = nearval3(IV.tend,lookups,0);
-
-if cfg.verbose; fprintf('%s: %d intervals in, %d intervals out\n',mfun,length(IV_temp.tstart),length(IV.tstart)); toc; end
+if cfg.verbose; fprintf('%s: %d intervals in, %d intervals out\n',mfun,num_intervals,length(IV.tstart)); toc; end
 
 % write history
-IV.cfg.history = IV_temp.cfg.history; % keep the history of the predecessor
 IV = History(IV,mfun,cfg);
 
 end
