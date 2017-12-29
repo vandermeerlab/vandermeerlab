5cd2a719d204c077dde04216e28312a16d554544
aacarey <carey.alyssa@gmail.com>
2017-12-29 12:50:24 -0500

    Helper function for getting list of rat IDs

---
+++ code-matlab/tasks/RippleDetection/helpers/SWRrats.m
@@ -0,0 +1,14 @@
+function rat_list = SWRrats
+%SWRRATS Get list of rat IDs for the SWR detection project
+%   rat_list = SWRrats
+%            = {'R050','R064','RBon','RCon'};
+%
+% aacarey Dec 2017
+
+mfun = mfilename;
+
+rat_list = {'R050','R064','RBon','RCon'};
+disp([mfun,': getting rat IDs for Digit, Obex, Bond, and Coriander...'])
+
+end
+
