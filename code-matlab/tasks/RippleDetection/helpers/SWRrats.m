9c62dc4db7ef8657075d2752b561437326354dfd
aacarey <carey.alyssa@gmail.com>
2018-01-01 20:03:06 -0500

    minor edit to SWRrats
    decided that it's not necessary that the first 4 characters of a filename be the rat's ID. code will handle rat ID whether it's 4-chars long (R050) or 3 chars long (bon).

--- code-matlab/tasks/RippleDetection/helpers/SWRrats.m
+++ code-matlab/tasks/RippleDetection/helpers/SWRrats.m
@@ -3,11 +3,16 @@ function rat_list = SWRrats
 %   rat_list = SWRrats
 %            = {'R050','R064','RBon','RCon'};
 %
+%   NOTE: this function contains a list of approved ripple project rats. To 
+%         add a new rat, open this function and edit the rat list. Ensure
+%         that all data and folders are structured as expected by the
+%         analysis software for the ripple detection project.
+%
 % aacarey Dec 2017
 
 mfun = mfilename;
 
-rat_list = {'R050','R064','RBon','RCon'};
+rat_list = {'R050','R064','bon','cor'};
 disp([mfun,': getting rat IDs for Digit, Obex, Bond, and Coriander...'])
 
 end
