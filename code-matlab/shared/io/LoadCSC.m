65f632ca8e0a6f0e4fd6e75aa596b79aee8c306e
mvdm <mvdm@dartmouth.edu>
2018-05-16 22:10:51 -0400

    Check for and remove colons in CSC header when loading
    Previous versions of Cheetah use colons (:) in the header, which caused the previous way of parsing the header to fail.

--- code-matlab/shared/io/LoadCSC.m
+++ code-matlab/shared/io/LoadCSC.m
@@ -214,13 +214,18 @@ for hline = 1:length(Header)
         continue;
     end
     
+    % if we are reading an older version header with colons, remove them
+    colon_idx = strfind(line,':');
+    if ~isempty(colon_idx)
+       line(colon_idx) = []; 
+    end
+    
     % This expression captures the first chunk of alphanumeric characters and puts it into
     % <key> then puts whatever is to the right of it into <val>. If there is only one
     % character chunk (e.g., missing value) then it returns <val> as empty.
     a = regexp(line(2:end),'(?<key>^\S+)\s+(?<val>.*)|(?<key>\S+)','names');
-    
+
     % deal with characters not allowed by MATLAB struct
-    
     if strcmp(a.key,'DspFilterDelay_ï¿½s') | strcmp(a.key,'DspFilterDelay_µs')
         a.key = 'DspFilterDelay_us';
     end
